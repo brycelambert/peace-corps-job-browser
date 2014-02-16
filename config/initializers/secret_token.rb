e1d7d52745c5629fdd387a9f399c2db7a77c3185
brycelambert <brycelambert@gmail.com>
2014-02-15 20:32:50 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeaceCorpsJobBrowser::Application.config.secret_key_base = '84fe41caca38ba6023267efc2b90be5d1b7ff60d0a7612c404e85c64cbbe6df82641bcbc38525368b8f990e33b8b15ed5fe36468635a13d2fc91885f4e4bdadf'
